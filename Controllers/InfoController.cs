65eedc5c977896156b6b340f13ee64ac48aec296
Obolenskiy Sergey <asp.entwickler@gmail.com>
2016-06-17 22:11:11 +0300

    InfoController added

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Windows_Service_Web_API_2_Sample.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class InfoController : ApiController
+    {
+        public string Get()
+        {
+            return "Windows Web API Sample Service On-Line (responced GET Method)";
+        }
+
+        public string Get(string param)
+        {
+            return "Windows Web API Sample Service On-Line (responced GET Method). Parameter = " + param;
+        }
+
+        public string Post()
+        {
+            return "Windows Web API Sample Service On-Line (responced POST Method)";
+        }
+
+        public string Post(string param)
+        {
+            return "Windows Web API Sample Service On-Line (responced POST Method). Parameter = " + param;
+        }
+
+    }
+
+}
